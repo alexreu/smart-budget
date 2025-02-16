15daa0044eca7252049f38dec4ff9cc493d1a5e8
Alexandre Adolphe <aadolphe@simplon.co>
2025-02-16 20:40:02 +0400

    Add wizard onboarding flow with currency selection and responsive UI components

---
+++ src/app/wizard/page.tsx
@@ -0,0 +1,64 @@
+import { CurrencyComboBox } from "@/components/CurrencyComboBox";
+import { Logo } from "@/components/Logo";
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import {
+    TypographyH1,
+    TypographyH2,
+    TypographyH3,
+} from "@/components/ui/typography";
+import { currentUser } from "@clerk/nextjs/server";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+export default async function WizardPage() {
+    const user = await currentUser();
+
+    if (!user) {
+        redirect("/sign-in");
+    }
+    return (
+        <div className="container flex flex-col max-w-2xl items-center justify-between gap-4">
+            <div className="text-center">
+                <TypographyH1>
+                    Welcome,{" "}
+                    <span className="ml-2 font-bold text-primary">
+                        {user.firstName}! 👋🏻
+                    </span>
+                </TypographyH1>
+                <TypographyH2 className="mt-4 text-muted-foreground">
+                    Let&apos;s get started by setting up your currency.
+                </TypographyH2>
+                <TypographyH3 className="mt-4 text-muted-foreground text-sm">
+                    You can change this setting as anytime later.
+                </TypographyH3>
+            </div>
+            <Separator />
+            <Card className="w-full">
+                <CardHeader>
+                    <CardTitle>Currency</CardTitle>
+                    <CardDescription>
+                        Select the currency you want to use.
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <CurrencyComboBox className="w-full" />
+                </CardContent>
+            </Card>
+            <Separator />
+            <Button className="w-full" asChild>
+                <Link href="/dashboard">I&apos;m ready to get started ✅</Link>
+            </Button>
+            <div className="mt-8">
+                <Logo />
+            </div>
+        </div>
+    );
+}
